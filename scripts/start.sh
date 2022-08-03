dd272dda76a97d9768e89a75a329699381ffff6d
Chad A. Woitas <woitas.chad@gmail.com>
2022-08-02 19:48:40 -0600

    Simplifying link creation

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-linuxcnc meca500-test.ini
+SCRIPT="\033[0;31mS\033[0;32mt\033[0;34ma\033[1;33mr\033[0;31mt\033[0m"
+
+echo -e "$SCRIPT: Initializing Variables"
+PKG_NAME="meca500"
+BASE_DIR=$(git rev-parse --show-toplevel)
+
+SRC_DIR="$BASE_DIR/src/"
+SHR_DIR="$BASE_DIR/share/"
+CFG_DIR="$BASE_DIR/linuxcnc/configs"
+DEB_DIR="$BASE_DIR/debian"
+
+MACH="sim-meca500"
+
+linuxcnc "$CFG_DIR/$MACH/$MACH.ini"
